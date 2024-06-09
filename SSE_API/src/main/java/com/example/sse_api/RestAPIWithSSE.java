007852a59c166646ece93c8ac8454b5f0acc2a0c
Kishore Kota <kishorekkota@gmail.com>
2024-06-09 10:28:53 -0500

    updates

--- SSE_API/src/main/java/com/example/sse_api/RestAPIWithSSE.java
+++ SSE_API/src/main/java/com/example/sse_api/RestAPIWithSSE.java
@@ -2,35 +2,31 @@ package com.example.sse_api;
 
 
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.codec.ServerSentEvent;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
-@RestController("/sse")
+import java.time.Duration;
+import java.time.LocalTime;
+
+@RestController
 @ComponentScan
 public class RestAPIWithSSE {
 
-
+    @CrossOrigin(origins = "http://localhost:8000")
     @GetMapping(value ="/test", produces = "text/event-stream")
-    public Flux serverSideEvent(){
+    public Flux<ServerSentEvent<String>> serverSideEvent(){
 
-        int count=0;
-        while(true)
-        {
-            try {
-                Thread.sleep(1000);
-                Flux.just("Hello", "World");
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            System.out.println("Hello");
-            count++;
-            if(count > 5)
-                break;
-        }
+        System.out.println("Event Stream called");
 
-
-        return Flux.just("Hello", "World");
+        return Flux.interval(Duration.ofSeconds(1))
+                .map(sequence -> ServerSentEvent.<String>builder()
+                        .id(String.valueOf(sequence))
+                        .event("update")
+                        .data("Server time: " + LocalTime.now())
+                        .build());
     }
 
 }
